f3236c4380a214743281578fe6d3b37df3ff68c1
Prince Singh <prince112592@yahoo.com>
2018-02-08 04:18:56 -0800

    Initial commit

---
+++ app/src/main/java/com/example/prince/myapplication/AddActivity.java
@@ -0,0 +1,41 @@
+package com.example.prince.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class AddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+
+        Button addBtn = (Button) findViewById(R.id.addBtn);
+        final EditText firstNumEditText = (EditText) findViewById(R.id.firstNumEditText);
+        final EditText secondNumEditText = (EditText) findViewById(R.id.secondNumEditText);
+        final TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
+
+        // Gets information from another activity
+        if (getIntent().hasExtra("com.example.prince.SWITCHSCREEN")){
+            String text = getIntent().getExtras().getString("com.example.prince.SWITCHSCREEN");
+            resultTextView.setText(text);
+        }
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                int num1 = Integer.parseInt(firstNumEditText.getText().toString());
+                int num2 = Integer.parseInt(secondNumEditText.getText().toString());
+                int result = num1 + num2;
+
+                resultTextView.setText(result + "");
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/prince/myapplication/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.prince.myapplication;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Launches another activity
+        Button secondActivityBtn = (Button) findViewById(R.id.secondActivityBtn);
+        secondActivityBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(getApplicationContext(), AddActivity.class);
+
+                startIntent.putExtra("com.example.prince.SWITCHSCREEN", "Your screen switched!");
+                startActivity(startIntent);
+            }
+        });
+
+
+        // Launches List activity
+        Button listActivityBtn = (Button) findViewById(R.id.listActivityBtn);
+        listActivityBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(getApplicationContext(), ListActivity.class);
+                startActivity(startIntent);
+            }
+        });
+
+
+
+        // Launch Navigation Activity
+        Button navigationActivityBtn = (Button) findViewById(R.id.navigationActivityBtn);
+        navigationActivityBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(getApplicationContext(), NavigationActivity.class);
+                startActivity(startIntent);
+            }
+        });
+
+
+
+
+        // Launch Activity (go to website) outside of app
+        Button googleBtn = (Button) findViewById(R.id.googleBtn);
+        googleBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                String google = "http://www.google.com";
+                Uri webAddress = Uri.parse(google);
+
+                Intent goToGoogle = new Intent(Intent.ACTION_VIEW, webAddress);
+                if (goToGoogle.resolveActivity(getPackageManager()) != null){
+                    startActivity(goToGoogle);
+                }
+            }
+        });
+
+    }
+}
