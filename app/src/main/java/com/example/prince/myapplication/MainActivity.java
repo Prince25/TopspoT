b34850ec2625d3c4da0fd8bc7586993ade0e015b
Prince Singh <prince112592@yahoo.com>
2018-02-09 01:05:40 -0800

    Update

--- app/src/main/java/com/example/prince/myapplication/MainActivity.java
+++ app/src/main/java/com/example/prince/myapplication/MainActivity.java
@@ -1,75 +1,69 @@
 package com.example.prince.myapplication;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout m_DrawerLayout;
+    private ActionBarDrawerToggle m_Toggle;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        m_DrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        m_Toggle = new ActionBarDrawerToggle(this, m_DrawerLayout, R.string.open, R.string.close);
+        m_DrawerLayout.addDrawerListener(m_Toggle);
+        m_Toggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        // Launches another activity
-        Button secondActivityBtn = (Button) findViewById(R.id.secondActivityBtn);
-        secondActivityBtn.setOnClickListener(new View.OnClickListener() {
 
-            @Override
+        // Launches Activities activity
+        Button allActivitiesBtn = (Button) findViewById(R.id.allActivitiesBtn);
+        allActivitiesBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                Intent startIntent = new Intent(getApplicationContext(), AddActivity.class);
-
-                startIntent.putExtra("com.example.prince.SWITCHSCREEN", "Your screen switched!");
-                startActivity(startIntent);
-            }
-        });
-
-
-        // Launches List activity
-        Button listActivityBtn = (Button) findViewById(R.id.listActivityBtn);
-        listActivityBtn.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                Intent startIntent = new Intent(getApplicationContext(), ListActivity.class);
+                Intent startIntent = new Intent(getApplicationContext(), Activities.class);
                 startActivity(startIntent);
             }
         });
 
 
 
-        // Launch Navigation Activity
-        Button navigationActivityBtn = (Button) findViewById(R.id.navigationActivityBtn);
-        navigationActivityBtn.setOnClickListener(new View.OnClickListener() {
+    }
 
-            @Override
-            public void onClick(View view) {
-                Intent startIntent = new Intent(getApplicationContext(), NavigationActivity.class);
-                startActivity(startIntent);
+
+
+    // For Navigation Menu Items
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (m_Toggle.onOptionsItemSelected(item)){
+            switch (item.getItemId()) {
+                case R.id.classrooms:
+                    Intent i = new Intent("com.test.demo.ABOUT");
+                    startActivity(i);
+                    break;
+                case R.id.lots:
+                    Intent p = new Intent("com.test.demo.PREFS");
+                    startActivity(p);
+                    break;
+                case R.id.settings:
+                    Intent g = new Intent("com.test.demo.PREFS");
+                    startActivity(g);
+                    break;
+                case R.id.quit:
+                    finish();
+                    break;
             }
-        });
-
-
-
-
-        // Launch Activity (go to website) outside of app
-        Button googleBtn = (Button) findViewById(R.id.googleBtn);
-        googleBtn.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                String google = "http://www.google.com";
-                Uri webAddress = Uri.parse(google);
-
-                Intent goToGoogle = new Intent(Intent.ACTION_VIEW, webAddress);
-                if (goToGoogle.resolveActivity(getPackageManager()) != null){
-                    startActivity(goToGoogle);
-                }
-            }
-        });
-
+            return false;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
