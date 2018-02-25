39392b794d1d6680a23825c98506ce630e03e5a9
Prince Singh <prince112592@yahoo.com>
2018-02-24 23:29:30 -0800

    Added partial database funtionality

---
+++ app/src/main/java/com/example/prince/myapplication/Classroom.java
@@ -0,0 +1,88 @@
+package com.example.prince.myapplication;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+/**
+ * Created by Prince on 2/24/2018.
+ */
+
+public class Classroom{
+
+    private String m_classroomName;
+
+    private int m_rows;
+    private int m_cols;
+    private int m_totalSeats;
+    private double m_seatNumber;
+
+    private DatabaseReference m_ClassroomDatabase = FirebaseDatabase.getInstance().getReference("Classroom");
+    private DatabaseReference m_CurrentClassroom;
+    private DatabaseReference m_Seat;
+
+    // Constructor
+    public Classroom(){}
+
+    public Classroom(String classroomName, int rows, int cols)
+    {
+        m_classroomName = classroomName;
+        m_rows = rows;
+        m_cols = cols;
+        m_totalSeats = m_rows * m_cols;
+
+        m_CurrentClassroom = m_ClassroomDatabase.child(m_classroomName);
+    }
+
+
+    // Main Functions
+    public boolean fillSeat(String name, int row, int col)
+    {
+        m_seatNumber = row * col;
+        if (m_seatNumber > 0 && m_seatNumber < m_totalSeats)
+        {
+            m_Seat = m_CurrentClassroom.child("Seat " + Integer.toString(row) + " " + Integer.toString(col));
+            m_Seat.child("Name").setValue(name);
+            m_Seat.child("Status").setValue(true);
+            return true;
+        }
+        else return false;
+    }
+
+    public boolean emptySeat(int row, int col)
+    {
+        m_seatNumber = row * col;
+        if (m_seatNumber > 0 && m_seatNumber < m_totalSeats)
+        {
+            m_Seat = m_CurrentClassroom.child("Seat " + Integer.toString(row) + " " + Integer.toString(col));
+            m_Seat.child("Name").setValue("");
+            m_Seat.child("Status").setValue(false);
+            return true;
+        }
+        else return false;
+    }
+
+
+    // Getters and Setters
+    //public String getName(int row, int col) { return m_name[row][col]; }
+   // public boolean getStatus(int row, int col) { return m_seat[row][col]; }
+
+    public String getClassroom() { return m_classroomName; }
+    public int getRows() { return m_rows; }
+    public int getCols() { return m_cols; }
+
+}
+
+
+
+
+/*m_CurrentClassroom.addValueEventListener(new ValueEventListener() {
+    @Override
+    public void onDataChange(DataSnapshot dataSnapshot) {
+
+    }
+
+    @Override
+    public void onCancelled(DatabaseError databaseError) {
+
+            }
+})*/
--- app/src/main/java/com/example/prince/myapplication/MainActivity.java
+++ app/src/main/java/com/example/prince/myapplication/MainActivity.java
@@ -8,11 +8,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout m_DrawerLayout;
     private ActionBarDrawerToggle m_Toggle;
+    Classroom BH3400 = new Classroom("BH 3400", 10, 20);
+    Classroom Moore100 = new Classroom("Moore 100", 40, 15);
+    Classroom Math5400 = new Classroom("Math 5400", 15, 10);
 
 
     @Override
@@ -25,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
         m_Toggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-
         // Launches Activities activity
         Button allActivitiesBtn = (Button) findViewById(R.id.allActivitiesBtn);
         allActivitiesBtn.setOnClickListener(new View.OnClickListener() {
@@ -35,6 +41,82 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+
+        Button takeBtn = (Button) findViewById(R.id.takeBtn);
+        Button emptyBtn = (Button) findViewById(R.id.emptyBtn);
+        final EditText nameTextEdit = (EditText) findViewById(R.id.nameTextEdit);
+        final EditText rowTextEdit = (EditText) findViewById(R.id.rowTextEdit);
+        final EditText colTextEdit = (EditText) findViewById(R.id.colTextEdit);
+
+        final Spinner dropdown = findViewById(R.id.classroomSpinner);
+        final String selectedItem = dropdown.getSelectedItem().toString();
+
+
+        takeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = nameTextEdit.getText().toString().trim();
+                int row = Integer.parseInt(rowTextEdit.getText().toString());
+                int col = Integer.parseInt(colTextEdit.getText().toString());
+                boolean result = true;
+
+                switch (selectedItem)
+                {
+                    case "BH 3400":
+                        if (!BH3400.fillSeat(name, row, col))
+                            result = false;
+                        break;
+
+                    case "Moore 100":
+                        if (!Moore100.fillSeat(name, row, col))
+                            result = false;
+                        break;
+
+                    case "Math 5400":
+                        if (!Math5400.fillSeat(name, row, col))
+                            result = false;
+                        break;
+                }
+
+                if (!result)
+                    Toast.makeText(getApplicationContext(), "Seat number is invalid", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        emptyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = nameTextEdit.getText().toString().trim();
+                int row = Integer.parseInt(rowTextEdit.getText().toString());
+                int col = Integer.parseInt(colTextEdit.getText().toString());
+                boolean result = true;
+
+                switch (selectedItem)
+                {
+                    case "BH 3400":
+                        if (!BH3400.emptySeat(row, col))
+                            result = false;
+                        break;
+
+                    case "Moore 100":
+                        if (!Moore100.emptySeat(row, col))
+                            result = false;
+                        break;
+
+                    case "Math 5400":
+                        if (!Math5400.emptySeat(row, col))
+                            result = false;
+                        break;
+                }
+
+                if (!result)
+                    Toast.makeText(getApplicationContext(), "Seat number is invalid", Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
 
 
     }
